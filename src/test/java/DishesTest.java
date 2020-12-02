32f40e82bc54884f37696b4608a5b32af9eea078
Ted Mosby <41184216+vovarudykk@users.noreply.github.com>
2020-12-02 13:16:43 +0200

    Add files via upload
    add DishesTest files (with toString() test)

---
+++ src/test/java/DishesTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class DishesTest {
+    private Plate.Size sizeOfPlate = new Plate.Size(0.5, 110);
+    private Dishes plate1 = new Plate("Plate1", Material.PORCELAIN, "China", sizeOfPlate);
+
+    @Test
+    public void toString_EqualString_True() {
+        Assertions.assertEquals("\nName: 'Plate1', Material: porcelain(country of production: France), Producer: China, Size(radius = 5.0мм; deep = 1.1м)'}",
+                plate1.toString());
+    }
+}
