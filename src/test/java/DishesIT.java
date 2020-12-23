694d52d8b2f53d335b27e57b3a96b2200d5fa8f2
Ted Mosby <41184216+vovarudykk@users.noreply.github.com>
2020-12-23 11:45:54 +0200

    Update and rename IDishesMockTest.java to DishesIT.java

--- src/test/java/DishesIT.java
+++ src/test/java/DishesIT.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-public class IDishesTest {
+public class DishesIT {
     private Plate.Size sizeOfPlate = new Plate.Size(0.5, 110);
     private SizeConvertor obj = new SizeConvertor();
 
@@ -13,5 +13,14 @@ public class IDishesTest {
         sizeOfPlate.convertSize(mock_obj);
         Assertions.assertEquals(sizeOfPlate.convertSize(obj), "radius = 5.0мм; deep = 1.1м");
         Mockito.verify(mock_obj).smToMm(sizeOfPlate.getRadius(), sizeOfPlate.getDeep());
+    }
+    
+    @Test
+    public void convertSize_CorrectReturnBySpy_True() {
+        SizeConvertor spy_obj = Mockito.spy(SizeConvertor.class);
+        Mockito.when(spy_obj.smToMm(sizeOfPlate.getRadius(), sizeOfPlate.getDeep())).thenReturn("radius = 5.0мм; deep = 1.1м");
+        sizeOfPlate.convertSize(spy_obj);
+        Assertions.assertEquals(sizeOfPlate.convertSize(obj), "radius = 5.0мм; deep = 1.1м");
+        Mockito.verify(spy_obj).smToMm(sizeOfPlate.getRadius(), sizeOfPlate.getDeep());
     }
 }
